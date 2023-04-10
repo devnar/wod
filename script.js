bf2250c5027ad9a8dab41a735159524d23e6f64f
NAR <yyazok@gmail.com>
2023-04-10 17:27:29 +0300

    v2.1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function wload() {
     const today = new Date();let h = today.getHours();let m = today.getMinutes();document.getElementById('clock').innerHTML =  h + ':' + m + '<br>' + today.getDate() + '.' + today.getMonth() + '.' + today.getFullYear();
     document.querySelector("#win1header").innerHTML = "<i class='icon n-terminal' style='margin-right: 16px;'></i>Terminal";
-    document.querySelector(".wincontent").innerHTML = "<div class='terminal'><div class='content'> <p class='command'>I'm not Hacker [V2.0]</p><br><div class='messages'></div><br><table> <tr> <td><span id='path' style='cursor:default;'>A:\User> </span></td><td width='100%'><input type='text' class='input-box' onkeydown='handleInput(event)'></td></tr></table> </div></div>";
+    document.querySelector(".wincontent").innerHTML = "<div class='terminal'><div class='content'> <p class='command'>I'm not Hacker [V2.1]</p><br><div class='messages'></div><br><table> <tr> <td><span id='path' style='cursor:default;'>A:\User> </span></td><td width='100%'><input type='text' class='input-box' onkeydown='handleInput(event)'></td></tr></table> </div></div>";
 }
 
 function cmd() {
     document.querySelector("#win1header").innerHTML = "<i class='icon n-terminal' style='margin-right: 16px;'></i>Terminal";
-    document.querySelector(".wincontent").innerHTML = "<div class='terminal'><div class='content'> <p class='command'>I'm not Hacker [V2.0]</p><br><div class='messages'></div><br><table> <tr> <td><span id='path' style='cursor:default;'>A:\User> </span></td><td width='100%'><input type='text' class='input-box' onkeydown='handleInput(event)'></td></tr></table> </div></div>";
+    document.querySelector(".wincontent").innerHTML = "<div class='terminal'><div class='content'> <p class='command'>I'm not Hacker [V2.1]</p><br><div class='messages'></div><br><table> <tr> <td><span id='path' style='cursor:default;'>A:\User> </span></td><td width='100%'><input type='text' class='input-box' onkeydown='handleInput(event)'></td></tr></table> </div></div>";
 }
 function mail() {
     document.querySelector("#win1header").innerHTML = "<i class='icon n-envelope' style='margin-right: 16px;'></i>E-posta";
@@ -15,4 +15,13 @@ function mail() {
 function edtr() {
     document.querySelector("#win1header").innerHTML = "<i class='icon n-edit' style='margin-right: 16px;'></i>Editor";
     document.querySelector(".wincontent").innerHTML = "Yakında";
+}
+
+function main() {
+    if (document.querySelector(".menu").style.display == "none") {
+        document.querySelector(".menu").style.display = "block"
+    } else {
+        document.querySelector(".menu").style.display = "none"
+    }
+    
 }
